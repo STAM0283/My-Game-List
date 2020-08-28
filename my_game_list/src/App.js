7c132bdea5c39f388b231cbbc85338c23c6a7f31
STAM0283 <aminestambouli00780@gemail.com>
2020-08-28 21:00:08 +0200

    the establishment of react router dom

--- my_game_list/src/App.js
+++ my_game_list/src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Main from "./components/Main";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-  
+      <BrowserRouter>
+        <Header />
+        <Main />
+      </BrowserRouter>
+      <Footer />
     </div>
   );
 }
--- my_game_list/src/components/Main.jsx
+++ my_game_list/src/components/Main.jsx
@@ -1,8 +1,24 @@
 import React from 'react';
+import Home from "./Home";
+import {Switch, Route} from "react-router-dom";
+import GameList from "./GameList";
+import MovieGame from "./MovieGame";
+
 
 const Main = () => {
     return (
         <div>
+       <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/game-list">
+            <GameList />
+          </Route>
+          <Route exact path="/game-clips">
+            <MovieGame />
+          </Route>
+        </Switch>
             
         </div>
     );
