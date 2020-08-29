45d74ee88fc8d1913c4542f78c86d05dd747b0fa
STAM0283 <aminestambouli00780@gemail.com>
2020-08-29 09:29:45 +0200

    add video component

--- my_game_list/src/components/NavBar.jsx
+++ my_game_list/src/components/NavBar.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import home_run from "../images/home_run.svg";
+import Game_icone from "../images/Game_icone.png";
+import Game_Clips from "../images/Game_Clips.png"
 import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div>
+    <div className = "navBar">
       <nav>
         <ul>
           <li>
             <NavLink exact activeClassName="current" to="/">
-              <h2>Home</h2>
+              <h2><img src = {home_run} alt = "Image Home" style ={{width: "20%"}}/>Home</h2>
             </NavLink>
           </li>
           <li>
             <NavLink exact activeClassName="current" to="/game-list">
-              <h2>Games List</h2>
+              <h2><img src = {Game_icone} alt = "Icone Game" />Games List</h2>
             </NavLink>
           </li>
           <li>
             <NavLink exact activeClassName="current" to="/game-clips">
-              <h2>Game Clips</h2>
+              <h2><img src = {Game_Clips} alt = "Icone Video Game" />Game Clips</h2>
             </NavLink>
           </li>
         </ul>
